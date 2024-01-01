598f9af1f7bc5ddb30aa0c718c58db341def7bff
1A3 <mcplayer1a3@gmail.com>
2024-01-01 16:26:21 +0000

    corrected target framework to 2.1

--- LobbyOffensiveNames/Plugin.cs
+++ LobbyOffensiveNames/Plugin.cs
@@ -7,95 +7,96 @@ using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
 
-namespace LobbyInviteOnly;
-
-[BepInPlugin(modGUID, "LobbyOffensiveNames", modVersion)]
-internal class PluginLoader : BaseUnityPlugin
+namespace LobbyInviteOnly
 {
-    private const string modGUID = "Dev1A3.LobbyOffensiveNames";
-
-    private readonly Harmony harmony = new Harmony(modGUID);
-
-    private const string modVersion = "1.0.1";
-
-    private static bool initialized;
-
-    public static PluginLoader Instance { get; private set; }
-
-    private void Awake()
+    [BepInPlugin(modGUID, "LobbyOffensiveNames", modVersion)]
+    internal class PluginLoader : BaseUnityPlugin
     {
-        if (initialized)
+        private const string modGUID = "Dev1A3.LobbyOffensiveNames";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private const string modVersion = "1.0.1";
+
+        private static bool initialized;
+
+        public static PluginLoader Instance { get; private set; }
+
+        private void Awake()
         {
-            return;
-        }
-        initialized = true;
-        Instance = this;
-        Assembly patches = Assembly.GetExecutingAssembly();
-        harmony.PatchAll(patches);
-
-        OffensiveNamesConfig.InitConfig();
-    }
-
-    public void BindConfig<T>(ref ConfigEntry<T> config, string section, string key, T defaultValue, string description = "")
-    {
-        config = ((BaseUnityPlugin)this).Config.Bind<T>(section, key, defaultValue, description);
-    }
-}
-internal class OffensiveNamesConfig
-{
-    public static ConfigEntry<bool> FilterEnabled;
-    public static ConfigEntry<string> FilterTerms;
-    public static string[] BlockedTermsRaw;
-
-    public static void InitConfig()
-    {
-        PluginLoader.Instance.BindConfig(ref FilterEnabled, "Settings", "Filter Enabled", true, "Should the offensive lobby name filter be enabled?");
-        PluginLoader.Instance.BindConfig(ref FilterTerms, "Settings", "Filter Terms", "nigger,nigga,n1g,nigers,negro,faggot,minors,chink,buttrape,molest,beastiality,cocks,cumshot,ejaculate,pedophile,furfag,necrophilia", "This should be a comma-separated list. Leaving this blank will also disable the filter.");
-        BlockedTermsRaw = FilterTerms.Value.Split(',').Select(x => x.Trim()).Where(x => x.Length > 0).ToArray();
-    }
-}
-
-[HarmonyPatch]
-internal static class SteamLobbyManagerStart_Patch
-{
-    [HarmonyPatch(typeof(SteamLobbyManager), "Start")]
-    [HarmonyPrefix]
-    private static void Prefix(ref SteamLobbyManager __instance)
-    {
-        __instance.censorOffensiveLobbyNames = OffensiveNamesConfig.FilterEnabled.Value && OffensiveNamesConfig.BlockedTermsRaw.Length > 0;
-    }
-}
-
-[HarmonyPatch]
-public static class loadLobbyListAndFilter_Patch
-{
-    [HarmonyPatch(typeof(SteamLobbyManager), "loadLobbyListAndFilter", MethodType.Enumerator)]
-    [HarmonyTranspiler]
-    public static IEnumerable<CodeInstruction> TranspileMoveNext(IEnumerable<CodeInstruction> instructions)
-    {
-        var newInstructions = new List<CodeInstruction>();
-        var skip = 0;
-        foreach (var instruction in instructions)
-        {
-            if (skip-- > 0) continue;
-
-            // check for IL_0021
-            if (instruction.opcode == OpCodes.Ldc_I4_S && (sbyte)instruction.operand == 21)
+            if (initialized)
             {
-                Debug.Log("Replaced offensiveWords");
-                // replace entire new array op codes with an array pointer
-                newInstructions.Add(new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(OffensiveNamesConfig), "BlockedTermsRaw")));
+                return;
+            }
+            initialized = true;
+            Instance = this;
+            Assembly patches = Assembly.GetExecutingAssembly();
+            harmony.PatchAll(patches);
 
-                // skip all array codes but keep:
-                // IL_00dd: stfld        string[] SteamLobbyManager/'<loadLobbyListAndFilter>d__15'::'<offensiveWords>5__2'
-                skip = 21 * 4 + 1;
+            OffensiveNamesConfig.InitConfig();
+        }
 
-                continue;
+        public void BindConfig<T>(ref ConfigEntry<T> config, string section, string key, T defaultValue, string description = "")
+        {
+            config = ((BaseUnityPlugin)this).Config.Bind<T>(section, key, defaultValue, description);
+        }
+    }
+    internal class OffensiveNamesConfig
+    {
+        public static ConfigEntry<bool> FilterEnabled;
+        public static ConfigEntry<string> FilterTerms;
+        public static string[] BlockedTermsRaw;
+
+        public static void InitConfig()
+        {
+            PluginLoader.Instance.BindConfig(ref FilterEnabled, "Settings", "Filter Enabled", true, "Should the offensive lobby name filter be enabled?");
+            PluginLoader.Instance.BindConfig(ref FilterTerms, "Settings", "Filter Terms", "nigger,nigga,n1g,nigers,negro,faggot,minors,chink,buttrape,molest,beastiality,cocks,cumshot,ejaculate,pedophile,furfag,necrophilia", "This should be a comma-separated list. Leaving this blank will also disable the filter.");
+            BlockedTermsRaw = FilterTerms.Value.Split(',').Select(x => x.Trim()).Where(x => x.Length > 0).ToArray();
+        }
+    }
+
+    [HarmonyPatch]
+    internal static class SteamLobbyManagerStart_Patch
+    {
+        [HarmonyPatch(typeof(SteamLobbyManager), "Start")]
+        [HarmonyPrefix]
+        private static void Prefix(ref SteamLobbyManager __instance)
+        {
+            __instance.censorOffensiveLobbyNames = OffensiveNamesConfig.FilterEnabled.Value && OffensiveNamesConfig.BlockedTermsRaw.Length > 0;
+        }
+    }
+
+    [HarmonyPatch]
+    public static class loadLobbyListAndFilter_Patch
+    {
+        [HarmonyPatch(typeof(SteamLobbyManager), "loadLobbyListAndFilter", MethodType.Enumerator)]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> TranspileMoveNext(IEnumerable<CodeInstruction> instructions)
+        {
+            var newInstructions = new List<CodeInstruction>();
+            var skip = 0;
+            foreach (var instruction in instructions)
+            {
+                if (skip-- > 0) continue;
+
+                // check for IL_0021
+                if (instruction.opcode == OpCodes.Ldc_I4_S && (sbyte)instruction.operand == 21)
+                {
+                    Debug.Log("Replaced offensiveWords");
+                    // replace entire new array op codes with an array pointer
+                    newInstructions.Add(new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(OffensiveNamesConfig), "BlockedTermsRaw")));
+
+                    // skip all array codes but keep:
+                    // IL_00dd: stfld        string[] SteamLobbyManager/'<loadLobbyListAndFilter>d__15'::'<offensiveWords>5__2'
+                    skip = 21 * 4 + 1;
+
+                    continue;
+                }
+
+                newInstructions.Add(instruction);
             }
 
-            newInstructions.Add(instruction);
+            return newInstructions.AsEnumerable();
         }
-
-        return newInstructions.AsEnumerable();
     }
 }
